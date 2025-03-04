c814cc9025fc2e148228fd8a8eef21002a167f89
Messias Olivindo <messias.olivindo@sou.inteli.edu.br>
2025-03-04 16:37:01 -0300

    Update CiberVentura

---
+++ scenes/inicial.js
@@ -0,0 +1,10 @@
+//Definir a cena inicial usando a biblioteca Phaser
+class inicial extends Phaser.Scene{
+    //Construtor da cena
+    constructor(){
+        super({
+            key: 'inicial',
+            backgroundColor: '#4D6AE8'
+        })
+    }
+}

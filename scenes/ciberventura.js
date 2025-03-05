6df4ae0d1ae3bef442e0b75274913933ff85e7ca
Messias Olivindo <messias.olivindo@sou.inteli.edu.br>
2025-03-04 23:14:19 -0300

    Scene 'inicial' finished and connecting to the 'ciberVentura' scene

--- scenes/ciberventura.js
+++ scenes/ciberventura.js
@@ -2,6 +2,12 @@ class CiberVentura extends Phaser.Scene{
     constructor(){
         super({
             key: 'ciberVentura',
+            
         })
+    }
+
+    create(){
+        this.cameras.main.setBackgroundColor("#e87373");
+        console.log("estouaqui");
     }
 }
--- scenes/inicial.js
+++ scenes/inicial.js
@@ -7,8 +7,53 @@ class Inicial extends Phaser.Scene {
             backgroundColor: '#4D6AE8'
         })
     }
-    create() {
-        console.log("Cena carregada")
 
+    //Carregar os elementos
+    preload() {
+        this.load.image('bgInicio', "../assets/bgInicio.png");
+        //this.load.image('logo', "assets/ciberVenturaLogo.png")
+    }
+
+    //Adicionar os elementos
+    create() {
+        //Adicionar um background que se move
+        this.bgCidade = this.add.tileSprite(this.game.config.width, this.game.config.height / 2, 2880, 1024, 'bgInicio').setScale(0.59);
+        //this.logo = this.add.image(400, 200, 'logo')
+
+        //Adicionar texto da logo
+        this.textoLogo = this.add.text(
+            this.game.config.width / 4,
+            100,
+            'CiberVentura',
+            {
+                fontSize: '60px', //tamanho da fonte
+                fill: '#211C84',  //cor
+                fontFamily: 'glitchRobot' //tipo da fonte
+            }
+        );
+
+        //Adicionar um retangulo interativo
+        this.retangulo = this.add.rectangle(this.game.config.width / 2, this.game.config.height / 2, 200, 50, 0x211C84).setOrigin(0.5).setInteractive(); //setInteractive indica que o elemento é interativo
+        this.retangulo.on('pointerdown', () => { //.on indica o que ocorrerá quando o elemento for clicado, para isso é necessário definir o 'pointerdown' para indicar o evento de clique
+            this.scene.start('ciberVentura', this.game); //Iniciar outra cena, sendo que o this.game guarda as informações obtidas nessa cena -uso futuro- 
+        });
+
+        //Adicionar um texto para iniciar o jogo
+        this.textoIniciar = this.add.text(
+            this.game.config.width / 2,
+            this.game.config.height / 2,
+            'Iniciar',
+            {
+                fontSize: "24px",
+                fill: '#ffffff',
+                fontFamily: 'glitchRobot'
+            }
+        ).setOrigin(0.5);
+
+    }
+
+    update() {
+        //Adiciona o movimento ao background
+        this.bgCidade.tilePositionX += 1;
     }
 }
